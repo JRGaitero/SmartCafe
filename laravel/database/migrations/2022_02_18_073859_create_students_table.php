4461f40b4fa7f630cffdcde274d277b61a0e5924
JRGaitero <jrgaitero.alumno@iesrafaelalberti.es>
2022-02-18 09:00:31 +0100

    user, student, school and cafe migrations

---
+++ laravel/database/migrations/2022_02_18_073859_create_students_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStudentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->string('name');
+            $table->string('surname');
+            $table->string('course');
+            $table->string('birthday');
+            $table->unsignedBigInteger('school_id');
+
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('school_id')->references('id')->on('schools');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('students');
+    }
+}
